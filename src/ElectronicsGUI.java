f32a1aa376272e8bb00c5bab4db5522307ff6f9d
deanogeorge13 <deanogeorge13@hotmail.com>
2018-11-08 15:53:37 +0000

    guis

--- src/ElectronicsGUI.java
+++ src/ElectronicsGUI.java
@@ -3,21 +3,28 @@ import javax.swing.*;
 public class ElectronicsGUI extends JFrame {
     public ElectronicsGUI()
     {
-        setSize(1000,1000);
+        setSize(500,400);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
 
         JMenuBar menuBar = new JMenuBar();
         setJMenuBar(menuBar);
-        JMenu menu = new JMenu("Shop");
-        JMenu menu1 = new JMenu()
+        JMenu menu = new JMenu("Store");
+        JMenu menu1 = new JMenu("Admin");
+        JMenu menu2 = new JMenu("test");
         menuBar.add(menu);
+        menuBar.add(menu1);
+        menuBar.add(menu2);
 
-        JMenuItem shop = new JMenuItem("Shop");
-        menu.add(shop);
+        JMenuItem computers = new JMenuItem("Computers");
+        JMenuItem consoles = new JMenuItem("Consoles");
+        JMenuItem displays = new JMenuItem("Displays");
+        menu.add(computers);
+        menu.add(consoles);
+        menu.add(displays);
 
-        JLabel label = new JLabel("Electronics Store");
-        add(label);
+        //JLabel label = new JLabel("Electronics Store");
+        //add(label);
         //JButton button = new JButton("Store");
 
 
