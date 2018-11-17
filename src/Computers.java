c7f8f0c863dd4e0bfe5e34c64be55f0e845ebcdc
deanogeorge13 <deanogeorge13@hotmail.com>
2018-11-17 21:31:19 +0000

    store object classes

---
+++ src/Computers.java
@@ -0,0 +1,9 @@
+public class Computers {
+    private String RAM;
+    private String CPU;
+    private String GPU;
+    private String Storage;
+    private String USB;
+    private String operatingSystem;
+    private String opticalDrive;
+}
---
+++ src/newDisplay.java
@@ -0,0 +1,4 @@
+public class newDisplay {
+
+    Displays BENQPD3200U = new Displays("32'", "3840x2160", "60Hz", "4ms", "HDMI, DisplayPort x2", );
+}
