5cbab0c74f7e6994fbc844c4329d50a966994f0b
deanogeorge13 <deanogeorge13@hotmail.com>
2018-11-19 13:51:51 +0000

    object classes

--- src/Computers.java
+++ src/Computers.java
@@ -6,4 +6,83 @@ public class Computers {
     private String USB;
     private String operatingSystem;
     private String opticalDrive;
+
+    public Computers(String RAM, String CPU, String GPU, String storage, String USB, String operatingSystem, String opticalDrive) {
+        this.RAM = RAM;
+        this.CPU = CPU;
+        this.GPU = GPU;
+        Storage = storage;
+        this.USB = USB;
+        this.operatingSystem = operatingSystem;
+        this.opticalDrive = opticalDrive;
+    }
+
+    @Override
+    public String toString() {
+        return "Computers{" +
+                "RAM='" + RAM + '\'' +
+                ", CPU='" + CPU + '\'' +
+                ", GPU='" + GPU + '\'' +
+                ", Storage='" + Storage + '\'' +
+                ", USB='" + USB + '\'' +
+                ", operatingSystem='" + operatingSystem + '\'' +
+                ", opticalDrive='" + opticalDrive + '\'' +
+                '}';
+    }
+
+    public String getRAM() {
+        return RAM;
+    }
+
+    public void setRAM(String RAM) {
+        this.RAM = RAM;
+    }
+
+    public String getCPU() {
+        return CPU;
+    }
+
+    public void setCPU(String CPU) {
+        this.CPU = CPU;
+    }
+
+    public String getGPU() {
+        return GPU;
+    }
+
+    public void setGPU(String GPU) {
+        this.GPU = GPU;
+    }
+
+    public String getStorage() {
+        return Storage;
+    }
+
+    public void setStorage(String storage) {
+        Storage = storage;
+    }
+
+    public String getUSB() {
+        return USB;
+    }
+
+    public void setUSB(String USB) {
+        this.USB = USB;
+    }
+
+    public String getOperatingSystem() {
+        return operatingSystem;
+    }
+
+    public void setOperatingSystem(String operatingSystem) {
+        this.operatingSystem = operatingSystem;
+    }
+
+    public String getOpticalDrive() {
+        return opticalDrive;
+    }
+
+    public void setOpticalDrive(String opticalDrive) {
+        this.opticalDrive = opticalDrive;
+    }
 }
